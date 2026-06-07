eb1e1d383ac908328eadceb3c0f341e237e1c92f
SethKkorir <zsethkipchumba179@gmail.com>
2026-06-07 10:35:11 +0300

    few changes

--- app.js
+++ app.js
@@ -30,11 +30,8 @@ app.use('/api/subjects', subjectRoutes);
 app.use('/api/scores', scoreRoutes);
 app.use('/api/results', resultsRoutes);
 
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, 'client')));
-
 // Fallback all non-API GET requests to index.html
-app.get('*', function (req, res, next) {
+app.get('/*', function (req, res, next) {
     if (req.path.startsWith('/api')) {
         return next();
     }
